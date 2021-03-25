729e811a188be8eb921c27698c47973a3bcf7372
Lovish <lovishsekhri3331>
2021-03-25 14:13:04 -0400

    Comments added

--- themes/index.php
+++ themes/index.php
@@ -1,2 +1,5 @@
 <?php
 // Silence is golden.
+// Lovish Sekhri
+// this is ecommerce website that has been targeted to potentially increase the sales of the cliet
+// the company that is helped through the mean of this website is Frost snownboards
