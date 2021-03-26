e44486cd9db8417ef03f6b592a6e5a018018897f
Lovish <lovishsekhri3331>
2021-03-25 22:21:36 -0400

    Post- types
    Styled and added in flex box to show the Cards

--- themes/sampletheme/footer.php
+++ themes/sampletheme/footer.php
@@ -10,51 +10,55 @@
  */
 
 ?>
+<div class="WP-post">
 
-<?php 
-
-$surfer_args = array (
-
-    'post_type' => 'lovishtheme_surfer',
-	'posts_per_page' => 3,
-
-);
-
-$surfer_query = new WP_Query($surfer_args);
-
-if($surfer_query->have_posts()){
-
-	while($surfer_query->have_posts()){
-		
-
-		$surfer_query->the_post()
-
-		?>
-		<h2 style="text-align:center;"><?php the_title();?></h2>
-		<section class="entry-content">
-		<?php the_post_thumbnail(); ?>
-        <?php the_excerpt(); ?>
 		<?php 
-			// Get the current post type
-		$postType = get_post_type();
-		echo '<a href="' . get_post_type_archive_link($postType) . '">Visit Post</a>';
+
+		$surfer_args = array (
+
+			'post_type' => 'lovishtheme_surfer',
+			'posts_per_page' => 3,
+
+		);
+
+		$surfer_query = new WP_Query($surfer_args);
+
+		if($surfer_query->have_posts()){
+
+			while($surfer_query->have_posts()){
+				
+
+				$surfer_query->the_post()
+
+				?>
+				<div class="Footer_WPQuery">
+				<h2 style="text-align:center;"><?php the_title();?></h2>
+				<section class="entry-content">
+				<?php the_post_thumbnail(); ?>
+				<?php the_excerpt(); ?>
+				<?php 
+					// Get the current post type
+				$postType = get_post_type();
+				echo '<a href="' . get_post_type_archive_link($postType) . '">Visit Post</a>';
+				?>
+
+			</section>
+			</div>
+				<?php
+				
+				
+				
+			}
+			
+		}
+
+
 		?>
-
-	</section>
-		<?php
-		
-		
-		
-	}
-	
-}
-
-
-?>
+</div>
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info site-footer">
-			<p>Copyright © 2020 Frost company. All rights reserved.</p>
+			<p style="width:100%; ">Copyright © 2020 Frost company. All rights reserved.</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
